1b93d3bcb1017eeabc9d64f18973606aa258f539
JPMetoyer <metoyerjeanpaul@gmail.com>
2024-01-13 18:26:16 -0600

    implemented admin panel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,7 @@ const [isMenu, setIsMenu] = useState(false)
   return (
 
 
-    <header className=' fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
+    <header className=' bg-primary fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
         {  /*  desktop & tablet */  }
         <div className="hidden md:flex w-full h-full items-center justify-between">
             <Link to={'/'} className='flex items-center gap-2'>
@@ -70,10 +70,14 @@ const [isMenu, setIsMenu] = useState(false)
             animate={{opacity: 1, x : 0}}
             exit={{opacity: 0, x : 200}} 
             className='flex items-center gap-8 ' >
-              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'> Home</li>
-              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'> Menu </li>
-              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'> About Us </li>
-              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'> Reservations  </li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'                 onClick={() => setIsMenu(false)}
+              > Home</li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'                 onClick={() => setIsMenu(false)}
+              > Menu </li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'                 onClick={() => setIsMenu(false)}
+              > About Us </li>
+              <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'                 onClick={() => setIsMenu(false)}
+              > Reservations  </li>
            </motion.ul>
 
             <div className='relative flex items-center justify-center'>
@@ -108,7 +112,10 @@ const [isMenu, setIsMenu] = useState(false)
                 user && user.email === "metoyerjeanpaul@gmail.com" && (
                   <Link to="/createItem">
                   <p 
-                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base "
+                  onClick={() => setIsMenu(false)}
+
+                  >
                   New Item 
                   <MdAdd/> 
                   </p>
@@ -175,7 +182,10 @@ const [isMenu, setIsMenu] = useState(false)
                 user && user.email === "metoyerjeanpaul@gmail.com" && (
                   <Link to="/createItem">
                   <p 
-                  className="px-4 py-2 flex items-center cursor-pointer gap-3 hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
+                  className="px-4 py-2 flex items-center cursor-pointer gap-3 hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base "
+                  onClick={() => setIsMenu(false)} 
+                  
+                  >
                   New Item 
                   <MdAdd/> 
                   </p>
@@ -186,10 +196,22 @@ const [isMenu, setIsMenu] = useState(false)
 
                <ul 
                 className='flex flex-col ' >
-                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> Home</li>
-                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 '> Menu </li>
-                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> About Us </li>
-                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> Reservations  </li>
+                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' 
+                onClick={() => setIsMenu(false)}
+                >
+                Home</li>
+                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 '
+                onClick={() => setIsMenu(false)}
+                > 
+                Menu </li>
+                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                onClick={() => setIsMenu(false)}
+                > 
+                About Us </li>
+                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                onClick={() => setIsMenu(false)}
+                >
+                 Reservations  </li>
               </ul>
 
 
